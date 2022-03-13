66dc38a5d5436c7368559035aa30b813ad87dadc
Жангелді Молдахан <crowndjoni@gmail.com>
2022-03-14 03:18:05 +0600

    add on maps current location

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import '../screens/login.dart';
-import '../services/user_service.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -13,7 +12,26 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int currenIndex = 0;
 
-  Completer<GoogleMapController> _controller = Completer();
+  late GoogleMapController googleMapController;
+  Set<Marker> markers = {};
+  // Completer<GoogleMapController> _controller = Completer();
+  // late GoogleMapController newGoogleMapController;
+  // late Position currentPosition;
+  // var geoLocator = Geolocator();
+
+  // void locatePosition() async {
+  //   Position position = await Geolocator.getCurrentPosition(
+  //       desiredAccuracy: LocationAccuracy.high);
+  //   currentPosition = position;
+
+  //   LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+
+  //   CameraPosition cameraPosition =
+  //       new CameraPosition(target: latLngPosition, zoom: 14);
+
+  //   newGoogleMapController
+  //       .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  // }
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(43.238949, 76.889709),
@@ -26,12 +44,29 @@ class _HomeState extends State<Home> {
       body: GoogleMap(
         mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
+        myLocationEnabled: true,
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: true,
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          googleMapController = controller;
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          Position position = await _determinePosition();
+
+          googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+              CameraPosition(
+                  target: LatLng(position.latitude, position.longitude),
+                  zoom: 14)));
+          markers.clear();
+
+          markers.add(Marker(
+              markerId: const MarkerId('currentLocation'),
+              position: LatLng(position.latitude, position.longitude)));
+
+          setState(() {});
+        },
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -53,5 +88,33 @@ class _HomeState extends State<Home> {
             },
           )),
     );
+  }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnable;
+    LocationPermission permission;
+
+    serviceEnable = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnable) {
+      return Future.error("Location services are disable");
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permission denied");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permission are permanently denied");
+    }
+
+    Position position = await Geolocator.getCurrentPosition();
+
+    return position;
   }
 }
